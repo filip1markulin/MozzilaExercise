a9d7466b1b770dd81f6764568b50779dfbc0dd51
filipmarkulin <you@example.com>
2021-01-11 09:03:16 +0100

    a

---
+++ cliss/oldtest/main.js
@@ -0,0 +1,27 @@
+function ComputedTemperature()
+{
+    let fahrenheit = document.getElementById("tempF").value
+    let fahrenheitNumber=Number(fahrenheit)
+    let celciusNumber= (fahrenheitNumber-32)/(9/5);
+    document.getElementById("Result").innerHTML = celciusNumber
+    if (celciusNumber<0)
+    {
+        alert("Its Freezing");
+    }
+    else{
+       if(celciusNumber<=10) 
+       {
+           alert("Its chilly");
+       }
+       else{
+           if(celciusNumber<=25)
+           {
+               alert("its normal");
+           }
+           else
+           {
+               alert("its Hot");
+           }
+       }
+    }
+}
